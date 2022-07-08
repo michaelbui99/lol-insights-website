12f750c5b92ef2bbf1619be624cff53f705c595f
Michael Bui <michaelbui99@hotmail.com>
2022-07-08 20:36:23 +0200

    Changed base url in config

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,4 +1,5 @@
-export const BASE_URL = 'http://localhost:8080/api/v1';
+export const BASE_URL =
+  'http://lol-insights.eu-west-1.elasticbeanstalk.com/api/v1';
 export const LOL_VERSION = '12.11.1';
 export const DDRAGON_IMG_CHAMPION_SQUARE_URL = `http://ddragon.leagueoflegends.com/cdn/${LOL_VERSION}/img/champion`;
-export const DDRAGON_IMG_PROFILE_ICON_URL = `http://ddragon.leagueoflegends.com/cdn/${LOL_VERSION}/img/profileicon`
+export const DDRAGON_IMG_PROFILE_ICON_URL = `http://ddragon.leagueoflegends.com/cdn/${LOL_VERSION}/img/profileicon`;
